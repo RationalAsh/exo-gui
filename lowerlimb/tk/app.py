bcba4751e2f3e884c2da6dea5fa4642fbe62c249
Ashwin Narayan <ashwinnarayan1994@gmail.com>
2022-06-24 14:20:01 +0800

    Wrote some code for the tkinter based user interface.

--- lowerlimb/tk/app.py
+++ lowerlimb/tk/app.py
@@ -1,25 +1,35 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter.ttk import Frame, Button, Style
 from tkinter.messagebox import showinfo
+from tkinter import RIGHT, BOTH, RAISED, font
 
 class Application(tk.Tk):
     def __init__(self):
         super().__init__()
 
         # configure the root window
-        self.title('Lower Limb Exoskeleton')
-        self.geometry('300x50')
+        self.title('Exo Manager')
+        self.geometry('320x240')
 
-        # label
-        self.label = ttk.Label(self, text='Hello, Tkinter!')
-        self.label.pack()
+        # Frame
+        self.frame = Frame(self, relief=RAISED, borderwidth=1)
+        self.frame.pack(fill=BOTH, expand=True)
 
-        # button
-        self.button = ttk.Button(self, text='Click Me')
-        self.button['command'] = self.button_clicked
-        self.button.pack()
+        default_font = font.Font(size=24, family='Courier')
 
-        self.attributes("-fullscreen", True)
+        # Label
+        msglabel = tk.Label(self.frame, text='Select an Option:')
+
+        # Listbox with options
+        options = tk.StringVar(value=('1. Record Experiment',
+                                      '2. Tune Parameters'))
+        self.listbox = tk.Listbox(self.frame, listvariable=options, height=2, selectmode='single', font=default_font)
+        self.listbox.activate(1)
+        self.listbox.pack(padx=1, pady=1)
+        self.listbox.select_set(0)
+
+        #self.attributes("-fullscreen", True)
 
     def button_clicked(self):
         showinfo(title='Information',
